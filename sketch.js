8a7eb4876092584455005b05c9e88b6fd56d9eaf
andrealoyola <andrea.loyolad@ucuenca.edu.ec>
2025-01-23 12:37:00 -0500

    Arreglo sketch.js

--- sketch.js
+++ sketch.js
@@ -1,61 +1,19 @@
-let posX, posY;
-let velX, velY; //velocidad
-let diametro;
-let radio;
-let bg;
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  radio = ceil(random(20, 50));
-  diametro = radio * 2;
-  posX = random(radio, width - radio);
-  posY = random(radio, height - radio);
-  velX = 6;
-  velY = 6;
-  bg = color(random(150, 0, 230), random(70, 0, 240), random(100, 10, 180));
-
-  //velX = random(-10, 10)
-  //velY = random(-10, 10)
 }
 
 function draw() {
-  background(bg);
-  posX += velX;
-  posY += velY;
-
-  //if (posX > width - radio || posX < radio)
-  //if(4<10 && 5>20) las 2 deben ser verdaderas para que se ejecute - || = OR basta que una de las 2 sea verdadera
-  if (posX > width - radio || posX < radio) {
-    velX *= -1;
-    bg = color(random(150, 0, 230), random(70, 0, 240), random(100, 10, 180));
-    //console.log("se fueeeee");
-  }
-  if (posY > height - radio || posY < radio) {
-    velY *= -1;
-    bg = color(random(150, 0, 230), random(70, 0, 240), random(100, 10, 180));
-  }
-  circle(posX, posY, diametro);
-  fill(255);
-  noStroke();
-}
-
-class Pelota{
-    //plano de instrucciones para crear una pelota
-    constructor(){
-        this.posX = random(rad, width)
-        this.posY
-        this.velX
-        this.velY
-        this.radio =
-        this.diametro =
-
+  let bgColor = color(random(20, 50), random(10, 30), random(20, 100));
+  background(bgColor);
+  let seguir = mouseX + 0.5;
+  let seguirY = mouseY + 0.5;
+  let circleSize = map(mouseX, 0, width, 20, 150); // tamaño en x
+  // Usando un for loop para dibujar círculos
+  for (let x = 0; x < width; x += 25) {
+    for (let y = 0; y < height; y += 50) {
+      fill(200, 20, 100, 60); // color y opacidad
+      noStroke();
+      circle(x, y, circleSize, 25, 20, 10, 10); // size circulos
     }
-
-    //metodo update
-    update(){
-
-    }
+  }
 }
-//nombre debe empezar con mayuscula siempre,no tiene ()
-// this -- eesta solo dentro de este objeto no dentro del todo el doc
